5c0f979e50b24c85dffebb3a5fba93aa8ea220ad
BSweet16 <blake.sweet16@gmail.com>
2025-12-29 04:10:05 -0500

    Update base url for vite production bundle

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 export default defineConfig({
   plugins: [react(),],
+  base: '/GraphicDesignAssociates/',  // Update to match Github repository name
   server: {
     port: 3000, // Use the same port as CRA (optional)
   },
