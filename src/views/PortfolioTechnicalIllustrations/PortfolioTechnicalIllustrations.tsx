2fd8593b6571f2233d76f6155aa6a71c14790184
BSweet16 <blake.sweet16@gmail.com>
2026-02-10 16:41:19 -0500

    Update descriptions and titles for pages

--- src/views/PortfolioTechnicalIllustrations/PortfolioTechnicalIllustrations.tsx
+++ src/views/PortfolioTechnicalIllustrations/PortfolioTechnicalIllustrations.tsx
@@ -13,55 +13,55 @@ const cardBlocks = [
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-1.jpg'),
         'Technical Illustration 1',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-1.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-2.jpg'),
         'Technical Illustration 2',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-2.pdf')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-3.jpg'),
         'Technical Illustration 3',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-3.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-4.jpg'),
         'Technical Illustration 4',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-4.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-5.jpg'),
         'Technical Illustration 5',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-5.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-6.jpg'),
         'Technical Illustration 6',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-6.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-7.jpg'),
         'Technical Illustration 7',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-7.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-8.jpg'),
         'Technical Illustration 8',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-8.jpg')
       ),
       new CardBlock(
         assetPath('assets/tech-illustrations/card-headers/tech-illustration-9.jpg'),
         'Technical Illustration 9',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/tech-illustrations/tech-illustration-9.jpg')
       ),
     ];
--- src/views/PortfolioTestimonials/PortfolioTestimonials.tsx
+++ src/views/PortfolioTestimonials/PortfolioTestimonials.tsx
@@ -13,19 +13,19 @@ const cardBlocks = [
       new CardBlock(
         assetPath('assets/testimonials/card-headers/testimonial-1.jpg'),
         'Testimonial 1',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/testimonials/testimonial-1.pdf'),
       ),
       new CardBlock(
         assetPath('assets/testimonials/card-headers/testimonial-2.jpg'),
         'Testimonial 2',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/testimonials/testimonial-2.pdf'),
       ),
       new CardBlock(
         assetPath('assets/testimonials/card-headers/testimonial-3.jpg'),
         'Testimonial 3',
-        'A beautiful view of the mountains during sunset.',
+        '',
         assetPath('assets/testimonials/testimonial-3.pdf'),
       ),
     ];
